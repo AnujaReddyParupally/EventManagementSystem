220a504ef14e316b835fe623895e136fe2394252
Anuja Reddy Parupally <panujareddy@deloitte.com>
2022-04-04 13:25:23 +0530

    Node constants

---
+++ server/utils/constants.js
@@ -0,0 +1,6 @@
+exports.RESPONSE_MSGS = {
+    ERROR_500: {CODE: 0, MSG: "Internal Server Error"},
+    OTP_FAILED: {CODE: 1, MSG: "Oops! Something went wrong. Please try again."},
+    EMAIL_NOT_FOUND: {CODE: 2, MSG: "Email not found"},
+    INVALID_USER: {CODE: 3, MSG: "Invalid User!"}
+  }
